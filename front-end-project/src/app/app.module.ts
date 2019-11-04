f8c5001fedccc8f724272becff6304bd25aa88dd
jeasonchan <439994526@qq.com>
2019-11-04 23:55:02 +0800

    使用materialModule一次性导入material库，并导出，再由app module导入使用。 ng serve --host 192.168.31.204，在局域网范围内可见。

--- front-end-project/src/app/app.module.ts
+++ front-end-project/src/app/app.module.ts
@@ -1,14 +1,21 @@
+import { MaterialModule } from './material/material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
